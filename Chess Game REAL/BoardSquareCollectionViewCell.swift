71821ab2da05e2685582c9b41adb4f514759b061
Nicholas Healy <nicholas.healy@smarkets.com>
2021-01-17 13:55:57 -0800

    initial commit

---
+++ Chess Game REAL/BoardSquareCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  BoardSquareCollectionViewCell.swift
+//  Chess Game REAL
+//
+//  Created by Josh Davis on 1/17/21.
+//
+
+import UIKit
+
+class BoardSquareCollectionViewCell: UICollectionViewCell {
+    let identifier = "BoardSquare"
+}
